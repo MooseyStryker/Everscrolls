88b88e5231166899c8f0cecb1dc0da20e751c4d6
Stryker Huber <stryker.important@gmail.com>
2024-03-28 12:28:30 -0700

    Reduce/thunk for tasks

--- react-vite/src/redux/tasks.js
+++ react-vite/src/redux/tasks.js
@@ -4,10 +4,10 @@ const PUT_TASK = "tasks/putTask";
 const DELETE_TASK = "tasks/deleteTask";
 
 
-const getAllTask = (noteId, task) => ({
+const getAllTask = (noteId, tasks) => ({
   type: GET_ALL_TASK,
   noteId,
-  task
+  tasks
 });
 
 const postTask = (noteId, task) => ({
@@ -50,3 +50,57 @@ export const thunkPostAllTask = (noteId, task) => async (dispatch) => {
         dispatch(postTask(noteId, data));
     }
 };
+
+
+export const thunkUpdateTask = (noteId, taskId, updateDescription) => async (dispatch) => {
+    const response = await fetch(`/api/notes/${noteId}/tasks/${taskId}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(updateDescription),
+    })
+
+
+    const data = await response.json();
+    if (data.errors) {
+       return data
+    } else {
+        dispatch(editTask(noteId, taskId, data));
+    }
+}
+
+export const thunkDeleteTask = (noteId, taskId) => async (dispatch) => {
+    const response = await fetch(`/api/notes/${noteId}/tasks/${taskId}`, {
+        method: "DELETE",
+    });
+    const data = await response.json();
+    if (data.errors) {
+        return data
+    } else {
+        dispatch(deleteTask(noteId, taskId, data));
+    }
+};
+
+const initialState = {};
+
+export default function notebodiesReducer(state = initialState, action) {
+    let newState;
+    switch (action.type) {
+        case GET_ALL_TASK:
+            let taskState = {}
+            action.tasks.forEach((task) => {
+                taskState[task.id] = task
+            })
+            return taskState
+        case POST_TASK_TO_NOTE:
+            return {...state, [action.noteId]: action.task
+            };
+        case PUT_TASK:
+            return {...state, [action.noteId]: action.task};
+        case DELETE_TASK:
+            newState = { ...state };
+            delete newState[action.taskId];
+            return newState;
+        default:
+          return state;
+    }
+}
